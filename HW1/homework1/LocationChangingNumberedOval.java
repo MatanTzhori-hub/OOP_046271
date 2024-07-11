67dd008c252f1d7d258609798da6fba938e15b58
Matan Tzhori <>
2024-07-11 23:28:46 +0300

    Added files for LocationChangingNumberedOval and LocationChangingRoundedRectangle

---
+++ HW1/homework1/LocationChangingNumberedOval.java
@@ -0,0 +1,16 @@
+package homework1;
+
+import java.awt.*;
+
+/**
+ * A LocationChangingNumberedOval is an Oval that have a unique number.
+ * The Oval can change its location using its step() method.
+ * A LocationChangingNumberedOval has a velocity property that determines the speed
+ * of location changing.
+ * Thus, a typical LocationChangingNumberedOval consists of the following set of
+ * properties: {location, color, dimension, size, velocity, number}
+ */
+
+public class LocationChangingNumberedOval extends LocationChangingOval {
+    
+}
---
+++ HW1/homework1/LocationChangingRoundedRectangle.java
@@ -0,0 +1,15 @@
+package homework1;
+
+import java.awt.*;
+
+/**
+ * A LocationChangingRoundedRectangle is a Rectangle with rounded corners that can change its location using its step()
+ * method. A LocationChangingRoundedRectangle has a velocity property that determines the speed
+ * of location changing.
+ * Thus, a typical LocationChangingRoundedRectangle consists of the following set of
+ * properties: {location, color, dimension, size, velocity}
+ */
+
+public class LocationChangingRoundedRectangle extends LocationChangingRectangle {
+    
+}
