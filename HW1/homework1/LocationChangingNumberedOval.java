020d27872b8742bdc8dd1a6efa5ab6b57f46ca7a
Matan Tzhori <>
2024-07-12 18:22:45 +0300

    Added implementations for LocationChangingNumberedOval and LocationChangingRoundedRectangle

--- HW1/homework1/LocationChangingNumberedOval.java
+++ HW1/homework1/LocationChangingNumberedOval.java
@@ -13,4 +13,74 @@ import java.awt.*;
 
 public class LocationChangingNumberedOval extends LocationChangingOval {
     
+    /**
+     * Abstraction Function: The LocationChangingNumberedOval class represents a numbered Oval
+     *                       that may move in the 2D plane.
+     *                       this.ovalNumber is the serial number of this oval.
+     *                       createdOvals are the total number of ovals that were created.
+     * 
+     * Representation Invariant: this.ovalNumber <= createdOvals
+     */ 
+
+    private static long createdOvals = 0;
+    private long ovalNumber;
+
+    private void checkRep() {
+        assert ovalNumber <= createdOvals: "Error: invalid oval number";
+    }
+
+    /**
+     * @requires location != null, color != null
+	 * @effects Initializes this with a a given location and color.
+	 *          bounding rectangle size is being set by default to (height, width) = (10, 10).
+	 */
+    public LocationChangingNumberedOval(Point location, Color color){
+        super(location, color);
+        this.ovalNumber = createdOvals++;
+        checkRep();
+    }
+
+    /**
+     * @requires location != null, color != null, dimension != null
+	 * @effects Initializes this with a a given location, color and dimension.
+     *          throws ImpossibleSizeException if got invalid dimension.
+	 */
+    public LocationChangingNumberedOval(Point location, Color color, Dimension dimension) throws ImpossibleSizeException{
+        super(location, color, dimension);
+        this.ovalNumber = createdOvals++;
+        checkRep();
+    }
+
+    /**
+     * @requires g != null
+     * @modifies g
+     * @effects Draws this onto g with this's number.
+     */
+    @Override
+    public void draw(Graphics g){
+        checkRep();
+        
+        int x = getLocation().x;
+        int y = getLocation().y;
+        int h = (int)getBounds().getWidth();
+        int w = (int)getBounds().getHeight();
+
+        super.draw(g);
+        ((Graphics2D) g).drawString(Long.toString(this.ovalNumber), (int)(x + w/2.0) , (int)(y + h/2.0));
+
+        checkRep();
+    }
+
+    /**
+     * @effects Creates and returns a copy of this.
+     */
+	@Override
+    public Object clone() {
+    	checkRep();
+    	LocationChangingNumberedOval ovalClone;
+    	ovalClone = (LocationChangingNumberedOval)super.clone();
+        ovalClone.ovalNumber = this.ovalNumber;
+        
+    	return ovalClone;
+    }
 }
--- HW1/homework1/LocationChangingRoundedRectangle.java
+++ HW1/homework1/LocationChangingRoundedRectangle.java
@@ -12,4 +12,67 @@ import java.awt.*;
 
 public class LocationChangingRoundedRectangle extends LocationChangingRectangle {
     
+    private static int ROUND_FACTOR = 6;
+
+    /**
+     * @effects Ensures the Rep. Invariant is kept, asserts otherwise.
+     */
+    private void checkRep() {
+        assert 1 < ROUND_FACTOR: "Error: invalid dimension";
+    }
+
+    /**
+     * @requires location != null, color != null
+	 * @effects Initializes this with a a given location and color.
+	 *          rectangle size is being set by default to (height, width) = (10, 10).
+	 */
+    public LocationChangingRoundedRectangle(Point location, Color color){
+        super(location, color);
+        checkRep();
+    }
+
+    /**
+     * @requires location != null, color != null, dimension != null
+	 * @effects Initializes this with a a given location, color and dimension.
+     *          throws ImpossibleSizeException if got invalid dimension.
+	 */
+    public LocationChangingRoundedRectangle(Point location, Color color, Dimension dimension) throws ImpossibleSizeException{
+        super(location, color, dimension);
+        checkRep();
+    }
+
+    /**
+     * @requires g != null
+     * @modifies g
+     * @effects Draws this onto g.
+     */
+    @Override
+    public void draw(Graphics g){
+        checkRep();
+
+        int x = getLocation().x;
+        int y = getLocation().y;
+        int h = (int)getBounds().getWidth();
+        int w = (int)getBounds().getHeight();
+
+        ((Graphics2D) g).setColor(getColor());
+    	((Graphics2D) g).fillRoundRect(x, y, w, h, w/ROUND_FACTOR , h/ROUND_FACTOR);
+
+        checkRep();
+    }
+
+    /**
+     * @effects Creates and returns a copy of this.
+     */
+	@Override
+    public Object clone() {
+        checkRep();
+
+    	LocationChangingRectangle rectClone;
+    	rectClone = (LocationChangingRectangle)super.clone();
+    	rectClone.dimension = (Dimension)dimension.clone();
+
+    	return rectClone;
+    }
+
 }
